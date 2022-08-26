84ce335bda0ea9288d7d0aee73fdc87757aec40e
patricia <patriciamedeiros41@gmail.com>
2022-08-26 16:09:09 -0300

    parte 2

---
+++ parte2.js
@@ -0,0 +1,20 @@
+// Parte 2 - Aprendendo sobre IF e ELSE
+
+// if (significa E SE)
+// else (significa SE NÃO)
+// == (significa comparação)
+// != (significa diferença)
+// <= (significa menor ou igual)
+// >= (significa maior ou igual)
+// < (significa menor)
+// > (significa maior)
+// {as chaves significam blocos}
+
+
+let idade = 27
+
+if(idade == 26) {
+    console.log("idade igual a 26")
+} else {
+    console.log("idade não é igual a 26")
+}
