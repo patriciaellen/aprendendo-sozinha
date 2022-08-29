f6b4d8740b9feed7aafdb69c3de7d0da8cb25549
patricia <patriciamedeiros41@gmail.com>
2022-08-29 14:06:30 -0300

    if e else e mais

--- parte2.js
+++ parte2.js
@@ -1,7 +1,10 @@
 // Parte 2 - Aprendendo sobre IF e ELSE
 
-// if (significa E SE)
+// if (significa SE) sim
 // else (significa SE NÃO)
+
+// COMANDOS CONDICIONAIS
+//            EXPRESSÃO RELACIONAL
 // == (significa comparação)
 // != (significa diferença)
 // <= (significa menor ou igual)
@@ -9,7 +12,11 @@
 // < (significa menor)
 // > (significa maior)
 // {as chaves significam blocos}
-
+// === (comando que verifica tudo, se é string ou number)
+//////////// ATENÇÃO SOBRE: === //////////
+// === ex: 1 = "1"
+/**  1 é number, "1" é string (entao quando for verificar vai ser false pois foram declarados de modos diferentes.)
+*/
 
 let idade = 27
 
@@ -17,4 +24,47 @@ if(idade == 26) {
     console.log("idade igual a 26")
 } else {
     console.log("idade não é igual a 26")
+}
+
+let idadeJogo = 18 
+
+if(idadeJogo >= 18) { // supondo que é um jogo, se a idade for MAIOR OU IGUAL A 18 A PESSOA PODE JOGAR.
+    console.log("Pode jogar")
+} else {
+    console.log("Acesso negado")
+}
+
+/** lembrando: BOOLEANS (PODEM SER TRUE OU FALSE) o valor de true é 1 e de false é 2.
+true valor 1
+false valor 0 
+*/ 
+
+let minhacaixa = 1 < 3 // LEMBRANDO UMA EXPRESSAO SEMPRE VAI TER UM RESULTADO
+console.log(minhacaixa)
+
+let minhacaixa1 = 1 > 3 // 1 não é maior que 3
+console.log(minhacaixa1) 
+
+let minhacaixa2 = 1 == 3 // 1 não é igual a 3
+console.log(minhacaixa2)
+
+/**
+Observando de outro modo: 
+ */
+ let nome = "João"
+ let idade1 = 10
+// considerando aqui que é maior ou igual a 18 anos.
+ if(idade1 <= 18) {
+    console.log(nome + " pode jogar.")
+} else {
+    console.log("acesso negado")
+}
+/**   Considerando aqui que é menor ou igual a 18 anos. 
+Usando TEMPLATE STRINGS
+para usar precisa usar a crase `${}`
+*/
+if(idade1 >= 18) {
+    console.log(`${nome} pode entrar na página. Você tem ${idade1} anos de idade`)
+} else {
+    console.log(`${nome} acesso negado.Você tem ${idade1} anos de idade`)
 }
